58672172d4bda622d60e508e67cc63ad050355c9
Your Name <carpentierodaniele@gmail.com>
2024-01-27 20:15:07 +0100

    feat: added info.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,10 @@
         <ul class="list-unstyled d-flex flex-wrap">
             @foreach ($comics as $comic)
                 <li class="col-md-2 mb-2 pe-3">
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid square-image ">
+                    <a href="/info">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid square-image ">
+                    </a>
+                    <h4> {{ $comic['series']}}</h4>
                 </li>
             @endforeach
         </ul>
